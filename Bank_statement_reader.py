7b969f11f852921f80d6cc3e3d0d6b6ad6c4c1e3
Cesare <cesaredoesit@gmail.com>
2017-12-18 18:37:35 -0600

    Day 3. ~5 hours.

--- Bank_statement_reader.py
+++ Bank_statement_reader.py
@@ -1,21 +1,57 @@
 # Figure out negative numbers when I do the checking/savings account. Have a statement for \-.
-# Refomat data so that it doesn't save to excel with a ' in front of every value. ' signifies text.
+
 import openpyxl
 import PyPDF2
 import re
 
-pdfFileObj = open("/home/cesare/Documents/11_23_17.pdf", "rb")
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-# pdfReader.numPages ----Reads number of pages...might use later.
-pageObj = pdfReader.getPage(2)  # Chooses what page to extract...need to iterate over each page except [1]
+
+def export_page_data():
+
+    pdfFileObj = open("/home/cesare/Documents/11_23_17.pdf", "rb")
+    pdfreader = PyPDF2.PdfFileReader(pdfFileObj)
+    number_of_pages = pdfreader.numPages
+    usable_pages = []
+    page_number = 0
+
+    while page_number < number_of_pages:
+        pageobj = pdfreader.getPage(page_number)
+        page = pageobj.extractText()
+        usable_pages.append(page)
+        page_number += 1
+    for item in usable_pages:
+        if "Description$ Amount" not in item:
+            del usable_pages[usable_pages.index(item)]
+
+    print(usable_pages)
+    return usable_pages
 
 
-def extract_usable_text(bank_statement):
+def extract_usable_text(extracted_pages):
 
-    split_text = bank_statement.split("Description$ Amount", 1)
-    shortened_string = str(split_text[1])
-    commas_deleted = shortened_string.replace(",", "")
-    return commas_deleted
+    edited_text = []
+
+    for item in extracted_pages:
+        text = item.replace(",", "")
+        payment_splitter = re.findall("Y*o*u*-M*o*b*i*l*e*-\d+.\d\d", text)
+
+        if "Description$ Amount" in text:
+            split_text = text.split("Description$ Amount", 1)
+            text = str(split_text[1])
+        if "Thank You-Mobile-" in text:
+            split_text = text.split(payment_splitter[0], 1)
+            text = str(split_text[1])
+        if "Total fees charged in" in text:
+            split_text = text.split("Total fees charged in", 1)
+            text = str(split_text[0])
+        if "CARDMEMBER SERVICEPO" in text:
+            split_text = text.split("CARDMEMBER SERVICEPO", 1)
+            text = str(split_text[0])
+        edited_text.append(text)
+
+    combined_text = "".join(edited_text)
+
+    print(combined_text)
+    return combined_text
 
 
 def extract_descriptions(text, dates, dollar_amounts):
@@ -30,12 +66,6 @@ def extract_descriptions(text, dates, dollar_amounts):
             values_replaced = values_replaced.replace(item, "XxXxX")
     values_replaced = values_replaced.split("XxXxX")
     values_replaced = values_replaced[:len(values_replaced)-1]
-    if "Payment Thank You" in values_replaced[0]:
-        first_item = values_replaced[0]
-        splitting_value = re.findall("\.\d\d", first_item)
-        payment_removed = first_item.split(splitting_value[0], 1)
-        string_payment_removed = str(payment_removed[1])
-        values_replaced[0] = string_payment_removed
 
     print(values_replaced)
     return values_replaced
@@ -43,15 +73,14 @@ def extract_descriptions(text, dates, dollar_amounts):
 
 def extract_dollar_amounts(text):
 
-    dollar_amounts = re.findall("\d+\.\d\d", text)
+    string_dollar_amounts = re.findall("\d+\.\d\d", text)
+    float_dollar_amounts = []
 
-    if "Total interest charged" in text:
-        dollar_amounts = dollar_amounts[:len(dollar_amounts)-2]
-    if "Payment Thank You" in text:
-        dollar_amounts = dollar_amounts[1:]
+    for item in string_dollar_amounts:
+        float_dollar_amounts.append(float(item))
 
-    print(dollar_amounts)
-    return dollar_amounts
+    print(float_dollar_amounts)
+    return string_dollar_amounts, float_dollar_amounts
 
 
 def extract_dates(text):
@@ -66,8 +95,6 @@ def extract_dates(text):
         else:
             shortened_dates.append(item[2:])
     del shortened_dates[1]
-    if "Payment Thank You" in text:
-        shortened_dates = shortened_dates[1:]
 
     print(shortened_dates)
     return shortened_dates
@@ -77,26 +104,28 @@ def export_to_excel(dates, descriptions, dollar_amounts):
 
     wb = openpyxl.load_workbook('/home/cesare/Documents/exported_bank_statement.xlsx')
     sheet = wb.get_sheet_by_name('Sheet1')
-    row = 1
+
+    row = 2
 
     for item in dates:
         sheet.cell(row=row, column=1, value=item)
         row += 1
-    row = 1
+    row = 2
     for item in descriptions:
-        sheet.cell(row=row, column=2, value=item)
-        row += 1
-    row = 1
-    for item in dollar_amounts:
         sheet.cell(row=row, column=3, value=item)
+        row += 1
+    row = 2
+    for item in dollar_amounts:
+        sheet.cell(row=row, column=4, value=item).number_format = "$#,##0.00"
         row += 1
 
     wb.save('/home/cesare/Documents/exported_bank_statement.xlsx')
 
 
-usable_text = extract_usable_text(pageObj.extractText())
+all_pages = export_page_data()
+usable_text = extract_usable_text(all_pages)
 usable_dates = extract_dates(usable_text)
 usable_dollar_amounts = extract_dollar_amounts(usable_text)
-usable_descriptions = extract_descriptions(usable_text, usable_dates, usable_dollar_amounts)
+usable_descriptions = extract_descriptions(usable_text, usable_dates, usable_dollar_amounts[0])
 
-export_to_excel(usable_dates, usable_descriptions, usable_dollar_amounts)
+export_to_excel(usable_dates, usable_descriptions, usable_dollar_amounts[1])
